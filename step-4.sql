d9d267a83aa6e34ce9b31ce8279effadacd91990
Tra <trab.die@gmail.com>
2022-05-08 23:04:28 -0400

    Work with Gordon

--- step-4.sql
+++ step-4.sql
@@ -1,42 +1,71 @@
--- select the park name, campground name, open_from_mm, open_to_mm & daily_fee ordered by park name and then campground name
-
 
 -- select the park name and the total number of campgrounds for each park ordered by park name
-
+SELECT p.name, COUNT (c.name) AS total_number_campground
+FROM park AS p
+JOIN campground AS c ON c.campground_id = p.park_id
+GROUP by p.name
+ORDER BY p.name;
 
 
 -- select the park name, campground name, site number, max occupancy, accessible, max rv length, utilities where the campground name is 'Blackwoods'
-
-/*
-  select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
-  -------------------------------------------------
-    Acadia	Blackwoods	276
-    Acadia	Seawall	198
-    Acadia	Schoodic Woods	92
-    Arches	"Devil's Garden"	49
-    Arches	Canyon Wren Group Site	1
-    Arches	Juniper Group Site	1
-    Cuyahoga Valley	The Unnamed Primitive Campsites	5
-  -------------------------------------------------
-*/
+SELECT p.name, c.name, s.site_number, s.max_occupancy,s.accessible, s.max_rv_length, s.utilities
+FROM park AS p
+JOIN campground AS c ON c.campground_id = p.park_id
+JOIN site AS s ON s.site_id = c.campground_id
+WHERE c.name = 'Blackwoods';
 
 
--- select site number, reservation name, reservation from and to date ordered by reservation from date
+-- /*
+--   -- select park name, campground, total number of sites (column alias 'number_of_sites') ordered by park
+--   -------------------------------------------------
+
+--     Acadia	Blackwoods	276
+--     Acadia	Seawall	198
+--     Acadia	Schoodic Woods	92
+--     Arches	"Devil's Garden"	49
+--     Arches	Canyon Wren Group Site	1
+--     Arches	Juniper Group Site	1
+--     Cuyahoga Valley	The Unnamed Primitive Campsites	5
+--   -------------------------------------------------
+-- */
+
+SELECT p.name, c.name, COUNT (s.site_id) AS number_of_sites
+FROM park AS p
+JOIN campground AS c ON c.park_id = p.park_id
+JOIN site AS s ON s.campground_id = c.campground_id
+GROUP BY p.name, c.campground_id
+ORDER BY p.name;
+
+-- -- select site number, reservation name, reservation from and to date ordered by reservation from date
+
+-- SELECT s.site_number, r.name, r.from_date, r.to_date
+-- FROM site AS s
+-- JOIN reservation AS r ON r.reservation_id = s.site_number
+-- ORDER BY r.from_date ;
 
 
+-- /*
+--   -- select campground name, total number of reservations for each campground ordered by total reservations desc
+--   -------------------------------------------------
+--     Seawall	13
+--     Blackwoods	9
+--     "Devil's Garden"	7
+--     Schoodic Woods	7
+--     The Unnamed Primitive Campsites	5
+--     Canyon Wren Group Site	4
+--     Juniper Group Site	4
+--   -------------------------------------------------
+-- */
 
-/*
-  select campground name, total number of reservations for each campground ordered by total reservations desc
-  -------------------------------------------------
-    Seawall	13
-    Blackwoods	9
-    "Devil's Garden"	7
-    Schoodic Woods	7
-    The Unnamed Primitive Campsites	5
-    Canyon Wren Group Site	4
-    Juniper Group Site	4
-  -------------------------------------------------
-*/
+
+SELECT c.name, COUNT(r.reservation_id) AS total_number_of_reservations
+FROM campground as c
+JOIN site AS s ON s.campground_id = c.campground_id
+JOIN reservation AS r ON r.site_id = s.site_id
+GROUP BY c.name
+ORDER BY total_number_of_reservations DESC;
+
+
 
 
 
--- step-5.sql
+++ step-5.sql
@@ -10,12 +10,16 @@
   ------------------------------
 */
 
+INSERT INTO park (name, location, establish_date, area, visitors, description)
+VALUES ('Ohiopyle', 'Pennsylvania', '1965-01-01', 19052, 1000000, 'Ohiopyle State Park is a Pennsylvania state park on 19,052 acres in Dunbar, Henry Clay and Stewart Townships, Fayette County, Pennsylvania in the United States. The focal point of the park is the more than 14 miles of the Youghiogheny River Gorge that passes through the park.');
+
 
 /*
   STEP TWO: You just found out that there was an error with the park data. Please update the park visitors to 1.5 million anually.
 
 */
 
+UPDATE park SET visitors = 1500000;
 
 /*
  STEP THREE: Insert new campground with the following data:
@@ -27,6 +31,9 @@
   daily_fee: 95.00
   ------------------------------------------------------------
 */
+
+INSERT INTO campground (park_id, name, open_from_mm, open_to_mm, daily_fee)
+VALUES (4, 'Youghiogheny', 01, 12, 95.00);
 
 
 /*
@@ -42,6 +49,15 @@
 */
 
 
+INSERT INTO site (site_number, campground_id)
+VALUES (623, 8);
+INSERT INTO site (site_number, campground_id)
+VALUES (624, 8);
+INSERT INTO site (site_number, campground_id)
+VALUES (625, 8);
+
+
+
 /*
  STEP FIVE: Insert 3 reservations, 1 for each site with the following data:
 ------------------------------------------------------------------------------------
@@ -52,15 +68,28 @@
 
 */
 
+INSERT INTO reservation (site_id, name, from_date, to_date)
+VALUES (623, 'Wayne Family', NOW() + INTERVAL '10 days', NOW() + INTERVAL '20 days');
+INSERT INTO reservation (site_id, name, from_date, to_date)
+VALUES (624, 'Parker Family', NOW() + INTERVAL '11 days', NOW() + INTERVAL '20 days');
+INSERT INTO reservation (site_id, name, from_date, to_date)
+VALUES (625, 'Kent Family', NOW() + INTERVAL '12 days', NOW() + INTERVAL '20 days');
+
+
 
 /*
  STEP SIX: The Wayne Family called and asked if they could change their reservation to today. Update the from_date to today and the to_date to a week from today.
 
  */
 
+UPDATE reservation SET from_date = NOW(), to_date = NOW() + INTERVAL '7 days'
+ WHERE name = 'Wayne Family';
+
 
 /*
  STEP SEVEN: The Kent family called and they would like to cancel their reservation. Delete the reservation for Kent Family.
+
+DELETE FROM reservation WHERE name = 'Kent Family';
 
 */
 
